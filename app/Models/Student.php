37e0195a40822ddb712ab0ab5e9b835c9f18de8b
group9project-grp <esystem.project2025@gmail.com>
2026-08-28 01:07:56 +0800

    feat: define Student model

---
+++ app/Models/Student.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Student extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'student_id',
+        'first_name',
+        'middle_name',
+        'last_name',
+        'email',
+        'mobile_number',
+        'gender',
+        'date_of_birth',
+        'program',
+        'year_level',
+        'address',
+        'profile_picture',
+    ];
+
+    protected $casts = [
+        'date_of_birth' => 'date',
+    ];
+
+    public function getFullNameAttribute(): string
+    {
+        return trim("{$this->first_name} {$this->middle_name} {$this->last_name}");
+    }
+}
